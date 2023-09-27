ce9fbfc82092b1ac3f929cfe020ad7583484ec3b
Mateuszdv <mateusd.vasc@gmail.com>
2023-09-26 22:08:34 -0300

    bug

--- Prática/Node.js/1_Intro/2_Primeiro_módulo/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs") //file system
-
-fs.readFile("/Prática/Node.js/1_Intro/2_Primeiro_módulo/arquivo.txt", "utf8", (err, data) => {
-  if (err) {
-    console.log(err)
-  }
-
-  console.log(data)
-})
